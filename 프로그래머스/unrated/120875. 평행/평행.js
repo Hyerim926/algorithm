da7e71768984784fb1692fd77109ef7ec80c6889
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2023-10-10 01:46:28 +0900

    [level unrated] Title: 평행, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.js
@@ -0,0 +1,26 @@
+function solution(dots) {
+  var answer = 0;
+
+    // 기울기 구하는 함수
+  function getInclination(a, b, c, d) {
+    let ab, cd;
+
+    ab = (b[1] - a[1]) / (b[0] - a[0]);
+    cd = (d[1] - c[1]) / (d[0] - c[0]);
+
+    if (ab === cd) {
+      answer += 1;
+    }
+  }
+
+  // 1-2, 3-4
+  getInclination(dots[0], dots[1], dots[2], dots[3]);
+
+  // 1-3, 2-4
+  getInclination(dots[0], dots[2], dots[1], dots[3]);
+
+  // 1-4, 2-3
+  getInclination(dots[0], dots[3], dots[1], dots[2]);
+
+  return answer > 0 ? 1 : 0;
+}
