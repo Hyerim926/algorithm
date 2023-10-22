468dd3e0ff2168d5daafe3d42753fc4fc0877aa2
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2023-10-23 01:20:45 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(my_string, s, e) {
+    var answer = '';
+    const reversed = my_string.slice(s, e + 1).split('').reverse().join('');
+    
+    answer = my_string.slice(0, s) + reversed + 
+        my_string.slice(e + 1, my_string.length)
+    return answer;
+}
