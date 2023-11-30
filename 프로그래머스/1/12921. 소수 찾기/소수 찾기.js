5ccb93d914d4227dd28f959a11e293dbbae59fd7
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2023-11-30 10:14:03 +0900

    [level 1] Title: 소수 찾기, Time: 124.66 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,29 @@
+const isPrime = (num) => {
+    if(num <= 1) {
+        return false;
+    }
+
+    if( num % 2 === 0) { 
+        return num === 2 ? true : false;
+    }
+   
+    const sqrt = parseInt(Math.sqrt(num));
+
+    for( let divider = 3; divider <= sqrt; divider += 2) {
+
+    if(num % divider === 0) {
+        return false;
+    }
+    
+  }
+  
+    return true;
+}
+
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i <= n; i++) {
+        if(isPrime(i)) answer++;
+    }
+    return answer;
+}
