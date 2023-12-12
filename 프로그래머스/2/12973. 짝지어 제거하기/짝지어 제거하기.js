2ed9e48a344cc3518f60b1dc1c89c8748aeb228c
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2023-12-12 14:04:29 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.04 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,16 @@
+function solution(s)
+{
+    var answer = -1;
+    
+    const stack = [];
+    for(let i = 0; i < s.length; i++) {
+        if(stack.length === 0 || stack[stack.length - 1] !== s[i]) {
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+        }
+    }
+    
+    answer = stack.length === 0 ? 1 : 0;
+    return answer;
+}
