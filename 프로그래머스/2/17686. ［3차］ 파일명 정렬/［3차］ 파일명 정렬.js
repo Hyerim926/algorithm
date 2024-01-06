2eb2403929666ee681c98cafd4414febdcbda9e9
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2024-01-07 01:59:43 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.96 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,18 @@
+function solution(files) {
+    var answer = [];
+    
+    answer = files
+          .map((file) => file.match(/(\D+)(\d+)/))
+          .sort((a, b) => {
+            if (a[1].toLowerCase() > b[1].toLowerCase()) { 
+              return 1;
+            } else if (a[1].toLowerCase() < b[1].toLowerCase()) {
+              return -1;
+            } else { 
+              return parseInt(a[2]) - parseInt(b[2]);
+            }
+          })
+          .map((file) => file.input);
+    
+    return answer;
+}
