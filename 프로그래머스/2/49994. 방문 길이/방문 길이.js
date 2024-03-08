e6454e834bd406c3c75211eb7320b17cddda8254
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2024-03-08 20:53:36 +0900

    [level 2] Title: 방문 길이, Time: 0.84 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,40 @@
+const MAP_LENGTH = 10;
+const BOUNDARY = MAP_LENGTH / 2;
+
+function solution(dirs) {
+    var answer = 0;
+    
+    const direction = {
+        'U' : [0, 1], 
+        'D' : [0, -1], 
+        'L' : [-1, 0], 
+        'R' : [1, 0]
+    };
+
+    const stack = [...dirs].reverse();
+    const set = new Set();
+    let [x, y] = [0, 0];
+
+    while (stack.length) {
+        const step = stack.pop();
+
+        const oldX = x;
+        const oldY = y;
+
+        x += direction[step][0];
+        y += direction[step][1];
+
+        if (x < -BOUNDARY || x > BOUNDARY || y < -BOUNDARY || y > BOUNDARY) {
+            x -= direction[step][0];
+            y -= direction[step][1];
+            continue;
+        }
+
+        set.add(`[${oldX}, ${oldY}] => [${x}, ${y}]`);
+        set.add(`[${x}, ${y}] => [${oldX}, ${oldY}]`);
+    }
+
+    answer = set.size / 2;
+    
+    return answer;
+}
