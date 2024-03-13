2f2a43d10360da42deedd9d7559071fc18e3b106
Hyerim Hwang <82012938+Hyerim926@users.noreply.github.com>
2024-03-13 19:58:29 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 12.78 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,32 @@
+function solution(queue1, queue2) {
+    var answer = -2;
+    
+    let tried = 0;
+    let queueLength = queue1.length + queue2.length;
+    let isFindSameQueue = false;
+    let idx1 = 0;
+    let idx2 = 0;
+    let queue1Sum = queue1.reduce((acc, val) => acc + val, 0);
+    let queue2Sum = queue2.reduce((acc, val) => acc + val, 0);
+
+    while(tried < queueLength * 2) {
+        if(queue1Sum > queue2Sum) {
+            const element = queue1[idx1++];
+            queue1Sum -= element;
+            queue2Sum += element;
+            queue2.push(element);
+        } else if(queue1Sum < queue2Sum) {
+            const element = queue2[idx2++];
+            queue1Sum += element;
+            queue2Sum -= element;
+            queue1.push(element);
+        } else {
+            isFindSameQueue = true;
+            break;
+        }
+        tried++;
+    }
+    
+    answer = isFindSameQueue ? tried : -1;
+    return answer;
+}
